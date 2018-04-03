1a8126c789ef4d3fc8908249a72311c99c335378
Dmitriy Krayniy <krdima92@gmail.com>
2018-04-03 14:51:44 +0300

    changed MongoDbPersistence test

--- test/persistence/RolesMongoDbPersistence.test.ts
+++ test/persistence/RolesMongoDbPersistence.test.ts
@@ -1,4 +1,6 @@
-import { YamlConfigReader } from 'pip-services-commons-node';
+let process = require('process');
+
+import { ConfigParams } from 'pip-services-commons-node';
 
 import { RolesMongoDbPersistence } from '../../src/persistence/RolesMongoDbPersistence';
 import { RolesPersistenceFixture } from './RolesPersistenceFixture';
@@ -8,8 +10,19 @@ suite('RolesMongoDbPersistence', ()=> {
     let fixture: RolesPersistenceFixture;
 
     setup((done) => {
-        let config = YamlConfigReader.readConfig(null, './config/test_connections.yml', null);
-        let dbConfig = config.getSection('mongodb');
+        var MONGO_DB = process.env["MONGO_DB"] || "test";
+        var MONGO_COLLECTION = process.env["MONGO_COLLECTION"] || "roles";
+        var MONGO_SERVICE_HOST = process.env["MONGO_SERVICE_HOST"] || "localhost";
+        var MONGO_SERVICE_PORT = process.env["MONGO_SERVICE_PORT"] || "27017";
+        var MONGO_SERVICE_URI = process.env["MONGO_SERVICE_URI"];
+
+        var dbConfig = ConfigParams.fromTuples(
+            "collection", MONGO_COLLECTION,
+            "connection.database", MONGO_DB,
+            "connection.host", MONGO_SERVICE_HOST,
+            "connection.port", MONGO_SERVICE_PORT,
+            "connection.uri", MONGO_SERVICE_URI
+        );
 
         persistence = new RolesMongoDbPersistence();
         persistence.configure(dbConfig);
@@ -17,9 +30,13 @@ suite('RolesMongoDbPersistence', ()=> {
         fixture = new RolesPersistenceFixture(persistence);
 
         persistence.open(null, (err: any) => {
-            persistence.clear(null, (err) => {
+            if (err == null) {
+                persistence.clear(null, (err) => {
+                    done(err);
+                });
+            } else {
                 done(err);
-            });
+            }
         });
     });
     
