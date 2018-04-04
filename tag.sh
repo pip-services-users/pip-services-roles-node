7e1eb11b2d8012df79ec8c85bf45c61d673905d1
Dmitriy Krayniy <krdima92@gmail.com>
2018-04-04 11:49:36 +0300

    make scripts executable

--- tag.sh
+++ tag.sh
@@ -10,7 +10,7 @@ git config --global user.email "pipdevs@gmail.com"
 git config --global user.name "pipdeveloper" 
 
 git remote rm origin 
-git remote add origin "https://pipdeveloper:${GITHUB_API_KEY}@github.com/pip-services-content/${COMPONENT}.git"
+git remote add origin "https://pipdeveloper:${GITHUB_API_KEY}@github.com/pip-services-users/${COMPONENT}.git"
 
 # Set git tag
 git tag ${TAG} -a -m "Generated tag from TravisCI for build #$TRAVIS_BUILD_NUMBER"
