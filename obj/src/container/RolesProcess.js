45c15545fbdf530cc33ee80c9c888f0e915a588e
Dmitriy Krayniy <krdima92@gmail.com>
2018-08-20 13:54:08 +0300

    added DefaultRpcFactory

--- obj/src/container/RolesProcess.js
+++ obj/src/container/RolesProcess.js
@@ -2,10 +2,12 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const pip_services_container_node_1 = require("pip-services-container-node");
 const RolesServiceFactory_1 = require("../build/RolesServiceFactory");
+const pip_services_rpc_node_1 = require("pip-services-rpc-node");
 class RolesProcess extends pip_services_container_node_1.ProcessContainer {
     constructor() {
         super("roles", "User roles microservice");
         this._factories.add(new RolesServiceFactory_1.RolesServiceFactory);
+        this._factories.add(new pip_services_rpc_node_1.DefaultRpcFactory);
     }
 }
 exports.RolesProcess = RolesProcess;
--- src/container/RolesProcess.ts
+++ src/container/RolesProcess.ts
@@ -2,12 +2,14 @@ import { IReferences } from 'pip-services-commons-node';
 import { ProcessContainer } from 'pip-services-container-node';
 
 import { RolesServiceFactory } from '../build/RolesServiceFactory';
+import { DefaultRpcFactory } from 'pip-services-rpc-node';
 
 export class RolesProcess extends ProcessContainer {
 
     public constructor() {
         super("roles", "User roles microservice");
         this._factories.add(new RolesServiceFactory);
+        this._factories.add(new DefaultRpcFactory);
     }
 
 }
