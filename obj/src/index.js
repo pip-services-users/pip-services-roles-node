4df88cc39cc16966366767311453055e7b27d9be
pipdeveloper <pipdevs@gmail.com>
2019-04-22 11:14:43 +0000

    project build by Travis CI [skip ci]

--- obj/src/index.js
+++ obj/src/index.js
@@ -9,5 +9,5 @@ __export(require("./logic"));
 __export(require("./services/version1"));
 __export(require("./build"));
 __export(require("./container"));
-//module.exports = require('./container/RolesSenecaPlugin');
+//module.exports = require('./container/RolesSenecaPlugin'); 
 //# sourceMappingURL=index.js.map
