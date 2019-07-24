33c95441ec986f7b22b02c680ae0e78bfc2dc707
Sergey Seroukhov <seroukhov@Sergeys-MacBook-Pro.local>
2019-07-24 14:39:51 -0700

    Enabled tests

--- test/persistence/RolesMemoryPersistence.test.ts
+++ test/persistence/RolesMemoryPersistence.test.ts
@@ -1,23 +1,23 @@
-// import { RolesMemoryPersistence } from '../../src/persistence/RolesMemoryPersistence';
-// import { RolesPersistenceFixture } from './RolesPersistenceFixture';
+import { RolesMemoryPersistence } from '../../src/persistence/RolesMemoryPersistence';
+import { RolesPersistenceFixture } from './RolesPersistenceFixture';
 
-// suite('RolesMemoryPersistence', ()=> {
-//     let persistence: RolesMemoryPersistence;
-//     let fixture: RolesPersistenceFixture;
+suite('RolesMemoryPersistence', ()=> {
+    let persistence: RolesMemoryPersistence;
+    let fixture: RolesPersistenceFixture;
     
-//     setup((done) => {
-//         persistence = new RolesMemoryPersistence();
-//         fixture = new RolesPersistenceFixture(persistence);
+    setup((done) => {
+        persistence = new RolesMemoryPersistence();
+        fixture = new RolesPersistenceFixture(persistence);
         
-//         persistence.open(null, done);
-//     });
+        persistence.open(null, done);
+    });
     
-//     teardown((done) => {
-//         persistence.close(null, done);
-//     });
+    teardown((done) => {
+        persistence.close(null, done);
+    });
         
-//     test('Get and Set Roles', (done) => {
-//         fixture.testGetAndSetRoles(done);
-//     });
+    test('Get and Set Roles', (done) => {
+        fixture.testGetAndSetRoles(done);
+    });
 
-// });
+});
